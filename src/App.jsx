978ab228d48a0284d9b7a7ba954958abe7a19eb9
yebology <yobelnathaniel12@gmail.com>
2024-07-08 16:03:30 +0700

    progress table admin for order

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import NavigationBar from "./components/admin/NavigationBar";
+import Home from "./pages/admin/Home";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen bg-blue-100 font-poppins overflow-hidden">
+      <NavigationBar />
+      <div className="px-12">
+        <Routes>
+          <Route path="/admin" element={<Home />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/services/ContentList.jsx
@@ -0,0 +1,39 @@
+const adminContent = [
+  {
+    id: "admin",
+    title: "Home",
+  },
+  {
+    id: "admin/rooms",
+    title: "Rooms",
+  },
+  {
+    id: "admin/facilities",
+    title: "Facilities",
+  },
+  {
+    id: "admin/beds",
+    title: "Beds",
+  },
+];
+
+const visitorContent = [
+    {
+        id: "",
+        title: "Home"
+    },
+    {
+        id: "rooms",
+        title: "Rooms"
+    },
+    {
+        id: "wishlist",
+        title: "Wishlist"
+    },
+    {
+        id: "history",
+        title: "History"
+    }
+];
+
+export { adminContent, visitorContent };
