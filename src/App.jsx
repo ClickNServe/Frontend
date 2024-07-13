483e58f3aa6f70488cbb8439ee20053f1ecb79fe
yebology <yobelnathaniel12@gmail.com>
2024-07-13 09:33:23 +0700

    add route stuff

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import Facility from "./pages/Facility";
 import Bed from "./pages/Bed";
 import Room from "./pages/Room";
 import Footer from "./components/fixed/Footer";
+import UpdateRoom from "./pages/update/UpdateRoom";
+import CreateRoom from "./pages/create/CreateRoom";
+import UpdateFacility from "./pages/update/UpdateFacility";
+import CreateFacility from "./pages/create/CreateFacility";
+import UpdateBed from "./pages/update/UpdateBed";
+import CreateBed from "./pages/create/CreateBed";
+import CreateReservation from "./pages/create/CreateReservation";
 
 const App = () => {
   return (
@@ -14,15 +21,24 @@ const App = () => {
       <div className="w-72 fixed sidebar bg-white">
         <Sidebar />
       </div>
-      <div
-        className={`md:ml-72 bg-main-bg min-h-screen w-full`}
-      >
+      <div className={`md:ml-72 bg-main-bg min-h-screen w-full`}>
         <div>
           <Routes>
+            {/* reservation */}
             <Route path="/" element={<Home />} />
+            <Route path="/create_reservation" element={<CreateReservation />} />
+            {/* rooms */}
             <Route path="/rooms" element={<Room />} />
+            <Route path="/rooms/:id" element={<UpdateRoom />} />
+            <Route path="/create_room" element={<CreateRoom />} />
+            {/* facilities */}
             <Route path="/facilities" element={<Facility />} />
+            <Route path="/facilities/:id" element={<UpdateFacility />} />
+            <Route path="/create_facility" element={<CreateFacility />} />
+            {/* beds */}
             <Route path="/beds" element={<Bed />} />
+            <Route path="/beds/:id" element={<UpdateBed />} />
+            <Route path="/create_bed" element={<CreateBed />} />
           </Routes>
           <Footer />
         </div>
